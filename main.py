e256f4194269142a07839d58fefef0c49d683603
Alihasnat10 <aliistheflash@gmail.com>
2023-04-18 14:21:28 +0500

    First commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from config import opt
 import numpy as np
 import imageio
 import cv2
+
 if __name__ == "__main__":
     if opt.train_check:
         _ = extract_data()
@@ -14,12 +15,9 @@ if __name__ == "__main__":
         BATCH_SIZE = 32
         BUFFER_SIZE = 1000
         STEPS_PER_EPOCH = NUM_EXAMPLES // BATCH_SIZE
-
-    model_path = "pretrained/carvana_model.h5"
-    test_img_path = "test_images/29bb3ece3180_11.jpg" 
     
     if opt.display_check:
-        sample_image, sample_mask = imageio.imread(test_img_path), imageio.imread(test_img_path)
+        sample_image, sample_mask = imageio.imread(opt.test_img_path), imageio.imread(opt.test_img_path)
         display([sample_image, sample_mask])
 
     if opt.train_check:
@@ -30,9 +28,9 @@ if __name__ == "__main__":
 
     if opt.predict_check:
         if not opt.train_check:
-            unet = load_model(model_path)
+            unet = load_model(opt.model_path)
 
-        img = np.array([cv2.resize(imageio.imread(test_img_path), (opt.IMG_ROWS, opt.IMG_COLS))])/255.0
+        img = np.array([cv2.resize(imageio.imread(opt.test_img_path), (opt.IMG_ROWS, opt.IMG_COLS))])/255.0
         pred_mask = unet.predict(img)[0]
         bin_mask = 255. * cv2.resize(pred_mask, (opt.TEST_IMG_ROWS, opt.TEST_IMG_COLS))
-        display([img, bin_mask])
+        display([img[0], bin_mask])
